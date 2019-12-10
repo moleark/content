3c77be39fc7ebab4bc18d5912eb9f951bcf9a370
unknown <W359@wlrc.com>
2019-12-10 08:41:56 +0800

    12.10优化提交

--- out/index.js
+++ out/index.js
@@ -11,11 +11,10 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const ejs = __importStar(require("ejs"));
 const bodyParser = __importStar(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const config_1 = __importDefault(require("config"));
-const tool_1 = require("./db/mysql/tool");
+const pages_1 = require("./pages");
 const path = require('path');
 const fs = require('fs');
 const markdownIt = require('markdown-it'), md = new markdownIt();
@@ -51,31 +50,8 @@ const markdownIt = require('markdown-it'), md = new markdownIt();
     //app.engine('html', ejs.);
     //设置模板引擎的格式即运用何种模板引擎
     app.set("view engine", "html");
-    app.use('/hello', async (req, resp) => {
-        let ret = await tool_1.tableFromSql("select * from salestask$test.tv_mycustomer limit 10;");
-        let html = '<!DOCTYPE html><html lang = "en"> <head> <meta charset = "UTF-8" ><title> <%- title %> </title> <style type = "text/css" >h1 {color: blue;}</style></head><body> <div></div> <%- content %> </body></html>';
-        /**
-        ret.forEach(function (item) {
-            html += ejs.render('<div>dddd data <%= a %></div> <%- content %> <%= name %>', { a: 2, content: '<h1>Hello</h1>', name: item.mobile });
-        });
-        **/
-        let content = "# Hello I am markdown \n # 一级标题 \n ## 二级标题 \n ### 三级标题";
-        var mdresult = md.render(content);
-        let result = ejs.render(html, { title: '模板页面', content: mdresult });
-        resp.end(result);
-    });
-    app.use('/markdown', async (req, resp) => {
-        let html = '<!DOCTYPE html><html lang = "en"> <head> <meta charset = "UTF-8" ><title> <%- title %> </title> <style type = "text/css" >h1 {color: blue;}</style></head><body> <div></div> <%- content %> </body></html>';
-        fs.readFile("../contentmanagement/ts/dome.md", 'utf8', (err, data) => {
-            if (err)
-                return;
-            else {
-                let mdresult = md.render(data);
-                let result = ejs.render(html, { title: '模板页面', content: mdresult });
-                resp.end(result);
-            }
-        });
-    });
+    app.use('/hello', pages_1.hello);
+    app.use('/markdown', pages_1.markdown);
     // 监听服务
     let port = config_1.default.get('port');
     app.listen(port, async () => {
--- ts/index.ts
+++ ts/index.ts
@@ -4,7 +4,7 @@ import * as bodyParser from 'body-parser';
 import cors from 'cors';
 import config from 'config';
 import { tableFromSql } from './db/mysql/tool';
-
+import { hello, markdown } from "./pages";
 const path = require('path');
 const fs = require('fs');
 const markdownIt = require('markdown-it'), md = new markdownIt();
@@ -46,31 +46,9 @@ const markdownIt = require('markdown-it'), md = new markdownIt();
     //设置模板引擎的格式即运用何种模板引擎
     app.set("view engine", "html");
 
-    app.use('/hello', async (req: Request, resp: Response) => {
-        let ret = await tableFromSql("select * from salestask$test.tv_mycustomer limit 10;");
-        let html = '<!DOCTYPE html><html lang = "en"> <head> <meta charset = "UTF-8" ><title> <%- title %> </title> <style type = "text/css" >h1 {color: blue;}</style></head><body> <div></div> <%- content %> </body></html>';
-        /**
-        ret.forEach(function (item) {
-            html += ejs.render('<div>dddd data <%= a %></div> <%- content %> <%= name %>', { a: 2, content: '<h1>Hello</h1>', name: item.mobile });
-        });
-        **/
-        let content = "# Hello I am markdown \n # 一级标题 \n ## 二级标题 \n ### 三级标题";
-        var mdresult = md.render(content);
-        let result = ejs.render(html, { title: '模板页面', content: mdresult })
-        resp.end(result);
-    });
+    app.use('/hello', hello);
 
-    app.use('/markdown', async (req: Request, resp: Response) => {
-        let html = '<!DOCTYPE html><html lang = "en"> <head> <meta charset = "UTF-8" ><title> <%- title %> </title> <style type = "text/css" >h1 {color: blue;}</style></head><body> <div></div> <%- content %> </body></html>';
-        fs.readFile("../contentmanagement/ts/dome.md", 'utf8', (err, data) => {
-            if (err) return;
-            else {
-                let mdresult = md.render(data);
-                let result = ejs.render(html, { title: '模板页面', content: mdresult });
-                resp.end(result);
-            }
-        })
-    });
+    app.use('/markdown', markdown);
 
     // 监听服务
     let port = config.get<number>('port');
